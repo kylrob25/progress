c1c2952608822712bddcbe368097025f9447d38a
Kyle Roberts <kylrob25@icloud.com>
2024-01-22 12:17:46 +0000

    Start working on security for login/register

---
+++ src/main/java/me/krob/config/WebSecurityConfig.java
@@ -0,0 +1,10 @@
+package me.krob.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfiguration {
+}
